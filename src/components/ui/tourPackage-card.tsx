26353e870ce182386476e689376c5ad4e1c731a4
rushabhsmehta <rushabhsmehat@gmail.com>
2024-02-23 16:12:53 +0530

    .

--- src/components/ui/tourPackage-card.tsx
+++ src/components/ui/tourPackage-card.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import Currency from "@/components/ui/currency";
-import getLocations from "@/actions/get-location";
 import { Location, TourPackage } from "../../../types";
 import { LucidePhone, LucideStar } from "lucide-react";
 import { Button } from "./button";
