2fac65d18015820ca8585048e226ecbde456e4ce
rushabhsmehta <rushabhsmehat@gmail.com>
2024-02-23 12:37:48 +0530

    .

--- src/app/(routes)/cart/components/summary.tsx
+++ src/app/(routes)/cart/components/summary.tsx
@@ -4,10 +4,11 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
-import Button from "@/components/ui/button";
+
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
+import { Button } from "@/components/ui/button";
 
 const Summary = () => {
   const searchParams = useSearchParams();
