d0a477a5e06c9492c0ff30586f842071040c2009
rushabhsmehta <rushabhsmehat@gmail.com>
2024-03-21 19:11:21 +0530

    .

--- src/app/(routes)/destinations/page.tsx
+++ src/app/(routes)/destinations/page.tsx
@@ -7,10 +7,10 @@ export default async function DestinationsPage() {
 
   return (
     <div className="container mx-auto py-36 px-8">
-      <div className="grid lg:grid-cols-3 gap-6">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6">
         {destinations.map((destination) => (
-          <div key={destination.id} className="shadow-lg rounded-lg">
-              <Image src={destination.imageUrl} alt={destination.label} width={500} height={500} objectFit="contain " />
+          <div key={destination.id} className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl w-full h-64 px-4 pb-8 pt-40 max-w-sm mx-auto mt-4">
+              <Image src={destination.imageUrl} alt={destination.label} width={500} height={500} objectFit="contain" />
               <div className="absolute bottom-0 left-0 bg-gradient-to-r from-red-500 to-orange-500 px-4 py-2 text-white text-sm">
                 {destination.label}
               </div>
