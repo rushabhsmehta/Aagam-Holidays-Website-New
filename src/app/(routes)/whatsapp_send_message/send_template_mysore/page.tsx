11b9088b199a27c592a01f0ed01cd741978fbcb6
rushabhsmehta <rushabhsmehat@gmail.com>
2024-04-01 22:12:56 +0530

    .

--- src/app/(routes)/whatsapp_send_message/send_template_mysore/page.tsx
+++ src/app/(routes)/whatsapp_send_message/send_template_mysore/page.tsx
@@ -25,13 +25,13 @@ export default function Send_Message() {
             try {
                 const response = await axios.post(`${baseUrl}?${params.toString()}`);
                 console.log(response.data);
-                alert('Message has been sent');
+              //  alert('Message has been sent');
 
             } catch (error) {
                 console.error('Error sending WhatsApp message:', error);
             }
         }
-        window.close();
+      //  window.close();
     };
    fetchData();
    /*  return (
