9aba4e98ad6c716128d81f48bec8b7307dfb0acf
rushabhsmehta <rushabhsmehat@gmail.com>
2024-04-01 21:23:35 +0530

    .

--- src/app/(routes)/destinations/page.tsx
+++ src/app/(routes)/destinations/page.tsx
@@ -12,7 +12,7 @@ export default async function DestinationsPage() {
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6">
         {locations.map((location) => (
           <Link href={`/tourPackages/${location.id}`} key={location.id} className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl w-full h-64 px-4 pb-8 pt-40 max-w-sm mx-auto mt-4">
-              <Image src={location.imageUrl} alt={location.label} width={500} height={500} objectFit="contain" />
+              <Image src={location.imageUrl} alt={location.label} width={500} height={500} />
               <div className="absolute bottom-0 left-0 bg-gradient-to-r from-red-500 to-orange-500 px-4 py-2 text-white text-sm">
                 {location.label}
               </div>            
