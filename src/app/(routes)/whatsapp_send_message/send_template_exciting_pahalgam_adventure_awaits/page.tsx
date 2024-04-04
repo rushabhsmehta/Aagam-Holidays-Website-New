c097a3b1783d1076321d3c98b61fda027075c613
rushabhsmehta <rushabhsmehat@gmail.com>
2024-04-05 00:13:32 +0530

    .

--- src/app/(routes)/whatsapp_send_message/send_template_exciting_pahalgam_adventure_awaits/page.tsx
+++ src/app/(routes)/whatsapp_send_message/send_template_exciting_pahalgam_adventure_awaits/page.tsx
@@ -8,7 +8,7 @@ export default function Send_Message() {
     const fetchData = async () => {
        console.log('fetchData called');
         const baseUrl = 'https://send2.digital/whatsapp/api/sendwhatsapp_1.php';
-        const numbers = ['918980413633', '919824218518', '919724444701', '919978783238']; // Add more numbers as needed
+        const numbers = ['919724444701', '919978783238']; // Add more numbers as needed
 
         for (const number of numbers) {
              const params = new URLSearchParams({
