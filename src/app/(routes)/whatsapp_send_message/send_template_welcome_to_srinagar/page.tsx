5324194fc069b6b26a5107dab5a8312e1063f8c0
rushabhsmehta <rushabhsmehat@gmail.com>
2024-04-06 20:26:11 +0530

    .

--- src/app/(routes)/whatsapp_send_message/send_template_welcome_to_srinagar/page.tsx
+++ src/app/(routes)/whatsapp_send_message/send_template_welcome_to_srinagar/page.tsx
@@ -1,17 +1,19 @@
 'use client'
-import react from 'react';  
+import React, { useState } from 'react';  
 import axios from "axios";
 import { Button } from "@/components/ui/button";
 
 export default function Send_Message() {
+    const [isSending, setIsSending] = useState(false);
+    const [isSent, setIsSent] = useState(false);
 
     const fetchData = async () => {
-       console.log('fetchData called');
+        setIsSending(true);
         const baseUrl = 'https://send2.digital/whatsapp/api/sendwhatsapp_1.php';
         const numbers = ['919724444701', '919978783238']; // Add more numbers as needed
 
         for (const number of numbers) {
-             const params = new URLSearchParams({
+            const params = new URLSearchParams({
                 user_name: 'Aagam8788',
                 password: 'dhmag@9f',
                 template_name: 'message_welcome_to_srinagar',
@@ -25,16 +27,26 @@ export default function Send_Message() {
             try {
                 const response = await axios.post(`${baseUrl}?${params.toString()}`);
                 console.log(response.data);
+                setIsSent(true);
             } catch (error) {
                 console.error('Error sending WhatsApp message:', error);
+            } finally {
+                setIsSending(false);
             }
         }
     };
-  // fetchData();
-     return (
+
+    if (isSent) {
+        alert('Message sent successfully');
+        window.close();
+    }
+
+    return (
         <div>
             <h1>Send Message</h1>
-            <Button className = "mt-40 ml-20 mb-20" onClick={fetchData}>Send Message</Button>
+            <Button className="mt-40 ml-20 mb-20" onClick={fetchData} disabled={isSending}>
+                Send Message
+            </Button>
         </div>
     ); 
 }
