2c22767cdb502586b990b6c1f22500131b3f309c
rushabhsmehta <rushabhsmehat@gmail.com>
2024-04-27 21:24:32 +0530

    .

--- src/components/ui/tourPackage-card.tsx
+++ src/components/ui/tourPackage-card.tsx
@@ -138,42 +138,33 @@ const TourPackageCard: React.FC<TourPackageCard> = ({
           </PopoverTrigger>
           <PopoverContent className="w-80">
             <div className="grid gap-4">
-              <div className="space-y-2">
-                <h4 className="font-medium leading-none">Contact Us</h4>
-                <p className="text-sm text-muted-foreground">
-                  Please fill out the form below.
-                </p>
+              <div className="grid items-center gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  className="h-8"
+                />
               </div>
-              <div className="grid gap-2">
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="name">Name</Label>
-                  <Input
-                    id="name"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="mobile">Mobile Number</Label>
-                  <Input
-                    id="mobile"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="message">Message</Label>
-                  <Input
-                    id="message"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <Button variant="default" className="col-span-3">Submit</Button>
+              <div className="grid items-center gap-2">
+                <Label htmlFor="mobile">Mobile Number</Label>
+                <Input
+                  id="mobile"
+                  className="h-8"
+                />
+              </div>
+              <div className="grid items-center gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  className="h-8"
+                />
+              </div>
+              <div className="grid items-center gap-2">
+                <Label htmlFor="message">Message</Label>
+                <Input
+                  id="message"
+                  className="h-8"
+                />
               </div>
             </div>
           </PopoverContent>
