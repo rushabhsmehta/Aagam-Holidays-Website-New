61b3da351de9b248474917dda3c1d988a2d8b621
rushabhsmehta <rushabhsmehat@gmail.com>
2024-05-21 21:59:26 +0530

    .

--- src/actions/get-all-tourPackages.tsx
+++ src/actions/get-all-tourPackages.tsx
@@ -25,7 +25,7 @@ const getalltourPackages = async (query: Query): Promise<TourPackage[]> => {
     },
   }); */
 
-  const res = await fetch(`${URL}`, { next: { revalidate: 60 } });
+  const res = await fetch(`${URL}`);
 
   return res.json();
 };
