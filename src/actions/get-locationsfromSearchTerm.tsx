890233bb69707416c14287ef6932186b0c9fa5bf
rushabhsmehta <rushabhsmehat@gmail.com>
2024-05-31 19:11:17 +0530

    .

--- src/actions/get-locationsfromSearchTerm.tsx
+++ src/actions/get-locationsfromSearchTerm.tsx
@@ -4,6 +4,7 @@ const URL=`${process.env.NEXT_PUBLIC_API_URL}/searchTermLocations`;
 
 const getLocationsBySearchTerm = async (searchTerm: string): Promise<Location[]> => {
   const res = await fetch(`${URL}/${searchTerm}`);
+  console.log(res.json());
 
   return res.json();
 };
