70b1f2bbe6326d24e64e8d765d63308c2d620852
rushabhsmehta <rushabhsmehat@gmail.com>
2024-06-02 18:14:05 +0530

    .

--- src/app/(routes)/searchPage/[searchTerm]/page.tsx
+++ src/app/(routes)/searchPage/[searchTerm]/page.tsx
@@ -11,14 +11,17 @@ interface SearchPageProps {
         searchTerm: string;
     },
 }
-
 const SearchPage: React.FC<SearchPageProps> = ({ params }) => {
     const title = `Search Results for: ${params.searchTerm}`;
     const [items, setItems] = useState<Location[]>([]);
     const [tourPackages, setTourPackages] = useState<TourPackage[][]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
+            setError(null);
+            setLoading(true);
             try {
                 const fetchedItems = await getLocationsFromSearchTerm(params.searchTerm);
                 setItems(fetchedItems);
@@ -30,12 +33,22 @@ const SearchPage: React.FC<SearchPageProps> = ({ params }) => {
                 );
                 setTourPackages(fetchedTourPackages);
             } catch (error) {
+                setError(error as string | null);
                 console.error('Error fetching data:', error);
             }
+            setLoading(false);
         };
 
         fetchData();
     }, [params.searchTerm]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
     return <LocationList title={title} items={items} tourPackages={tourPackages} />;
 };
