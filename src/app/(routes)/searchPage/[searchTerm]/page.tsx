4bb14c8b1a57eb6e5b701fdf8e89fa831603f737
rushabhsmehta <rushabhsmehat@gmail.com>
2024-06-02 14:58:27 +0530

    .

--- src/app/(routes)/searchPage/[searchTerm]/page.tsx
+++ src/app/(routes)/searchPage/[searchTerm]/page.tsx
@@ -6,26 +6,26 @@ import LocationList from '@/components/location-list';
 
 interface SearchPageProps {
     params: {
-        searchTerm : string;
+        searchTerm: string;
     },
 }
 
 const SearchPage: React.FC<SearchPageProps> = async ({ params }) => {
     const title = `Search Results for: ${params.searchTerm}`;
-  
+
     try {
-      const items = await getLocationsFromSearchTerm(params.searchTerm);
-      const tourPackages = await Promise.all(
-        items.map(async (item) => {
-          return await getTourPackages({ locationId: item.id });
-        })
-      );
-  
-      return <LocationList title={title} items={items} tourPackages={tourPackages} />;
+        const items = await getLocationsFromSearchTerm(params.searchTerm);
+        const tourPackages = await Promise.all(
+            items.map(async (item) => {
+                return await getTourPackages({ locationId: item.id });
+            })
+        );
+
+        return <LocationList title={title} items={items} tourPackages={tourPackages} />;
     } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle the error gracefully, e.g., display an error message to the user
-      return <div>Error fetching data. Please try again later.</div>;
+        console.error('Error fetching data:', error);
+        // Handle the error gracefully, e.g., display an error message to the user
+        return <div style={{ marginTop: 20 }}>Error fetching data. Please try again later.</div>;
     }
-  };
+};
 export default SearchPage;
